dd55b8412dd10e90bbfb99efbcfa0d932a5d1ea8
jihoon-jang <jigns153@naver.com>
2024-10-06 23:43:19 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/cockcipe/cockcipe/CockcipeApplication.java
@@ -0,0 +1,13 @@
+package com.cockcipe.cockcipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CockcipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CockcipeApplication.class, args);
+	}
+
+}
